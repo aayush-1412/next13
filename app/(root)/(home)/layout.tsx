5b2a8330441fb947cf3f899da32b1203acad493d
Aayush <gaayushg11@gmail.com>
2024-01-23 17:55:28 +0530

    add auth

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react'
+// import './globals.css'
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
