c02a8f18bde326529d842e81d289651135b886ff
Aayush <gaayushg11@gmail.com>
2024-01-24 17:31:52 +0530

    added nabar and mobile navigatioin

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import React,{useContext,createContext,useState,useEffect} from "react"
 
 interface ThemeContextType {
@@ -6,16 +6,18 @@ interface ThemeContextType {
     setMode:(mode:string)=>void
 }
 
-const ThemeContext=createContext<ThemeContextType | undefined>(undefined);
+ const ThemeContext=createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({children}:{children:React.ReactNode}){
-    const [mode,setMode]=useState('')
+    const [mode,setMode]=useState("")
     const handleThemeChange=()=>{ 
-        if(mode==='dark') {
-            setMode('light');
-            document.documentElement.classList.add('light');
-        }else{
+        if(localStorage.theme==='dark' || (!("theme" in localStorage ) && window.matchMedia("(prefers-color-scheme:dark)").matches)) {
             setMode('dark');
             document.documentElement.classList.add('dark');
+            // document.documentElement.classList.remove('dark');
+        }else{
+            setMode('light');
+            document.documentElement.classList.remove('dark');
+            // document.documentElement.classList.remove('light');
         }
     }
     useEffect(()=>{
