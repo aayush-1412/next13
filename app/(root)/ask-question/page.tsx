fd052fb8e50bfe4da9a8996a088ff6006fff456f
Aayush <gaayushg11@gmail.com>
2024-01-27 21:16:57 +0530

    added ask question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Question from "@/components/forms/Question";
 
-const page = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ask-question | Dev Overflow",
+  description: "Ask quesiton page of Dev Overflow",
+};
+
+const AskQuestion = async () => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
   return (
-    <div>Ask questions</div>
-  )
-}
-
-export default page
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
+};
+export default AskQuestion;
