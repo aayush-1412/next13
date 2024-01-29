508189515cf901fc4f65c284ca08c6651a6f6143
Aayush <gaayushg11@gmail.com>
2024-01-30 01:09:29 +0530

    fixed middleware

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,6 @@
 import Question from "@/components/forms/Question";
 
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import type { Metadata } from "next";
@@ -12,17 +12,18 @@ export const metadata: Metadata = {
 };
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId="CL123"
+  const { userId } = auth();
+ 
+  // const userId="CL123"
 
   if (!userId) redirect("/sign-in");
   const mongoUser=await getUserById({userId})
-  console.log(mongoUser)
+  console.log(userId)
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
     </div>
   );
